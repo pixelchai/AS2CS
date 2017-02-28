b433a3560f8aac8e94db0ad67abc958d5d624b8a
pixelzerg <abhinavbhandari4@gmail.com>
2017-02-28 09:33:37 +0000

    updts

---
+++ AS2CS/AS2CS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PygmentSharp.Core;
+using PygmentSharp.Core.Lexing;
+
+namespace AS2CS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var highlighted = Pygmentize.Content("var moo = new {Console.WriteLine(\"jadoijad\")}").WithLexer(new CSharpLexer()).GetTokens();
+        }
+    }
+}
