045aca2fa68a0e1b07e2c58f18a3ad2865d1c31b
PixelZerg <abhinavbhandari4@gmail.com>
2017-03-29 15:21:49 +0100

    updt hanging

---
+++ AS2CS/AS2CS/Nodes/Bracket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2CS.Nodes
+{
+    public class Bracket : Node
+    {
+        public Bracket(TokenStream tokenStream) : base(tokenStream)
+        {
+        }
+
+        public override Node Select()
+        {
+            //Expression will use this
+        }
+    }
+}
