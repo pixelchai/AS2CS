c3c340ff628c4096b40e6c4dfa9f98c42cfa76a3
PixelZerg <abhinavbhandari4@gmail.com>
2017-04-29 23:39:15 +0100

    implemented PostfixOperator

---
+++ AS2CS/AS2CS/Nodes/PostfixOperator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2CS.Nodes
+{
+    public class PostfixOperator : Node
+    {
+        public PostfixOperator(TokenStream tokenStream) : base(tokenStream)
+        {
+        }
+
+        public override Node Select()
+        {
+            if (Accept(new TokenNode(ts, "", "+")) && Accept(new TokenNode(ts, "", "+"))) return this;
+            if (Accept(new TokenNode(ts, "", "-")) && Accept(new TokenNode(ts, "", "-"))) return this;
+            return null;
+        }
+    }
+}
