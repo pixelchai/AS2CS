10c3b57c7a0a58a288c581f26bfee10297470d8c
PixelZerg <abhinavbhandari4@gmail.com>
2017-04-30 14:02:47 +0100

    Workspace Close

---
+++ AS2CS/AS2CS/Nodes/BExpr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2CS.Nodes
+{
+    /// <summary>
+    /// added - bridge to Expr. Use this.
+    /// </summary>
+    public class BExpr
+    {
+    }
+}
